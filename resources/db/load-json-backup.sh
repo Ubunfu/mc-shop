b29b80f0d315381d4e836fb9e1179e9c8286eabc
Ryan Allen <rallen3882@gmail.com>
2021-04-01 17:42:57 -0400

    [RAllen] Fixes to help generate updated dataload easier

--- resources/db/load-json-backup.sh
+++ resources/db/load-json-backup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+export TABLE_NAME=$1
+export SCAN_OUTPUT_FILE=$2
+
+if [[ -z "$TABLE_NAME" ]]; then
+    echo "missing a parameter"
+    exit 1
+fi
+if [[ -z "$SCAN_OUTPUT_FILE" ]]; then
+    echo "missing a parameter"
+    exit 1
+fi
+
+node makeBatch.js
+
+aws dynamodb batch-write-item --request-items file://items_0.json
 aws dynamodb batch-write-item --request-items file://items_1.json
 aws dynamodb batch-write-item --request-items file://items_2.json
 aws dynamodb batch-write-item --request-items file://items_3.json
@@ -7,4 +22,3 @@ aws dynamodb batch-write-item --request-items file://items_4.json
 aws dynamodb batch-write-item --request-items file://items_5.json
 aws dynamodb batch-write-item --request-items file://items_6.json
 aws dynamodb batch-write-item --request-items file://items_7.json
-aws dynamodb batch-write-item --request-items file://items_8.json
