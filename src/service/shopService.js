5d77af76175911313da3d27af678bcf52889507f
Ryan Allen <rallen3882@gmail.com>
2021-06-22 20:07:03 -0400

    Revert "Proxying error message detail when wallet charges fail"
    This reverts commit fd0e25d6957905e869ab636681f28e4f1aab259c.

--- src/service/shopService.js
+++ src/service/shopService.js
@@ -45,7 +45,7 @@ async function buyItem(player, itemName, quantity) {
         await walletService.chargeWallet(player, totalCost);
     } catch (err) {
         log('[shopService] error charging wallet: ' + err.message);
-        throw Error(err.message);
+        throw Error('error charging wallet');
     }
 
     try {
