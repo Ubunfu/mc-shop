c8561a105660d350f720f90debf7a5d9e6ded2cf
Ryan Allen <rallen3882@gmail.com>
2021-06-24 23:39:24 -0400

    implementing get all items service

--- src/service/itemService.js
+++ src/service/itemService.js
@@ -14,7 +14,14 @@ async function getItem(docClient, itemName) {
 }
 
 async function getItems(docClient) {
-
+    let params = {
+        TableName: process.env.TABLE_ITEMS
+    }
+    const items = await docClient.scan(params).promise()
+    if (items.Items == undefined) {
+        return [];
+    }
+    return items.Items;
 }
 
 exports.getItem = getItem;
